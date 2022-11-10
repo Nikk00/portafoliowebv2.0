5425981214c9295d394669cf9440d77e907c2ffb
Nicolas Luza <43099709+Nikk00@users.noreply.github.com>
2022-11-10 20:00:19 -0300

    v1.3.2

--- pages/components/Tooltip.js
+++
@@ -1,8 +0,0 @@
-const Tooltip = (prop) =>{
-    return(
-        <div id="tooltip-animation" role="tooltip" className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip">
-            {prop.text}
-            <div className="tooltip-arrow" data-popper-arrow></div>
-        </div>
-    )
-}
